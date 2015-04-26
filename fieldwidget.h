8c66d1afb218030aa5ca8ff3aafbdb01bd79aebb
koskiszz <koskiszz@gmail.com>
2015-04-26 23:28:26 +0300

    Добавлен класс для виджета и как-то нарисована формочка

---
+++ fieldwidget.h
@@ -0,0 +1,17 @@
+#ifndef FIELDWIDGET_H
+#define FIELDWIDGET_H
+
+#include <QWidget>
+
+class FieldWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit FieldWidget(QWidget *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // FIELDWIDGET_H
--- life.pro
+++ life.pro
@@ -13,8 +13,10 @@ TEMPLATE = app
 
 
 SOURCES += main.cpp\
-        mainwindow.cpp
+        mainwindow.cpp \
+    fieldwidget.cpp
 
-HEADERS  += mainwindow.h
+HEADERS  += mainwindow.h \
+    fieldwidget.h
 
 FORMS    += mainwindow.ui
